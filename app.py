de65bd0e5471bedc069cdfe6abcde8075f008b75
studygianella-netizen <studygianella@gmail.com>
2026-05-21 22:29:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,17 @@ st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Gianella Zorrilla Quispe")
 st.write ("Curso: Especialización en Python potenciado con IA - Edición - 58")
+st.write ("2026")
 
 menu = st.sidebar.selectbox("Seleccione una opción",
     ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
 
 if menu == "Home":
     st.write("Bienvenido a mi aplicación")
-
+    st.write("Descripción del proyecto:")
+    st.write("Aplicación interactiva desarrollada en Streamlit como proyecto aplicado del módulo de Fundamentos de Programación. El proyecto integra conceptos esenciales de Python como variables, estructuras de datos, control de flujo, funciones, programación funcional y programación orientada a objetos (POO), mediante una interfaz dinámica e intuitiva.")
+    st.write("Tecnologías utilizadas:")
+    st.write("Tecnologías utilizadas:")
 elif menu == "Ejercicio 1":
     st.title("Flujo de Caja")
 
