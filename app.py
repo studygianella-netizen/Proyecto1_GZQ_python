3909264e8e36fdadd48fa695b33ea6c11cf4828d
studygianella-netizen <studygianella@gmail.com>
2026-05-21 21:43:07 -0500

    Refactor app.py to maintain functionality

--- app.py
+++ app.py
@@ -15,38 +15,6 @@ if sesion == "Sesión 1":
   st.write("Bienvenido la sesión 1")
   st.image("Image20260512194059.png" )
 
-elif sesion == "Sesión 2":
-  st.write("Bienvenido la sesión 2")
 
-  precio = st.number_input("Ingrese el precio del producto", min_value = 0 , max_value = 5000 , value = 1200)
-  descuento = st.number_input("Ingrese el descuento del producto del 0 al 100% ", min_value = 0 , max_value = 100 )
-
-  precio_final_producto = precio - (precio*(descuento/100))
-
-  st.write("El precio final del producto es: ", precio_final_producto  )
-
-
-elif sesion == "Sesión 3":
-
-    st.write("Bienvenido la sesión 3")
-
-    fin_rango = st.slider(
-        "Seleccione un valor",
-        min_value=0,
-        max_value=20,
-        value=7
-    )
-
-    arreglo = np.arange(0, fin_rango)
-
-    st.write(arreglo)
-
-else:
-   st.write("Bienvenido a la sesión 4")
-   principal = st.number_input("Ingrese el monto del préstamo", value=1000)
-   tasa_anual = st.number_input("Ingrese la tasa anual en decimal", value=0.1, min_value=0.0, max_value=1.0)
-   anios = st.number_input("Ingrese el número de años del préstamo", value=1)
-   pagos_anio = st.number_input("Ingrese la cantidad de pagos por año", value=12)
-    
    cuota = round(lf.cuota_prestamo(principal, tasa_anual, anios, pagos_anio),2)
    st.write(f"El valor de la cuota es {cuota}")
