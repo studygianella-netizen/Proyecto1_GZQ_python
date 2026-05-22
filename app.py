3de58ee07e1f5a95e9ded99777051ab3285ae15f
studygianella-netizen <studygianella@gmail.com>
2026-05-21 22:00:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,19 @@ import numpy as np
 import libreria_funciones_proyecto1 as lf
 st.set_page_config(page_title="Proyecto 1 Python", layout="wide")
 
+st.sidebar.image("python.png")
+
+st.title("PROYECTO 1")
+
 st.sidebar.title("Parámetros")
 
 st.write("Elaborado por: Gianella Zorrilla Quispe")
-st.write ("Especialización en Python potenciado con IA - Edición - 58")
+st.write ("Curso: Especialización en Python potenciado con IA - Edición - 58")
 
-menu = st.sidebar.selectbox(
-    "Seleccione una opción",
-    ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"]
-)
+menu = st.sidebar.selectbox("Seleccione una opción",
+    ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
 
 if menu == "Home":
-    st.title("Proyecto Aplicado en Streamlit")
     st.write("Bienvenido a mi aplicación")
 
 elif menu == "Ejercicio 1":
